d6f088c1406e7c8ab294ec87a2800f4e3fe324f6
Bernadette Davis <bsd231@nyu.edu>
2020-02-01 16:38:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 
-const totalBatteries = reduce(batteryBatches, function(total, element){return total+element},0);
+const totalBatteries = batteryBatches.reduce(function(total, element){return total+element},0);
